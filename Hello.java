3f5305efa95032b77d257aae8c6fd788719d7f9d
Maneesha Karn <maneeshakarn@google.com>
2021-06-16 18:04:49 +0800

    My First Kokoro project prepwork

---
+++ Hello.java
@@ -0,0 +1,10 @@
+/**
+ * To test the setup realted to kokoro 
+ * 
+ */
+public class Hello {
+
+  public static void main(String[] argv) {
+    System.out.println("Something to Think about Maneesha !");
+  }
+}
